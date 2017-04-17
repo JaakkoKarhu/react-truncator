b63d08b777540bdcfdad58897e6a5d2d0172874c
Jaakko Karhu <jaakko.st.karhu@gmail.com>
2017-04-17 21:57:01 +0200

    fine search works

--- src/index.js
+++ src/index.js
@@ -25,114 +25,100 @@ class Truncator extends React.Component {
   constructor(props) {
     super (props)
     this.state = {
-      text: [],
-      preventInfiniteLoop: 0,
-      isFine: false
+      segments: [],
+      updateStopper: 0,
+      concatenated: ''
     }
   }
 
-  traverse = (arr) => {
-    let newArr = [ ...arr ]
-    let lolArr
-    for (let i = 0; i < 1; i++) {
-      lolArr = []
-      newArr.map((item, _i) => {
-        let size = newArr[_i]
-        let str = newArr[_i]
-        let length = Math.ceil(str.length/2)
-        let firstHalf = newArr[_i].substring(0, length);
-        let secondHalf = newArr[_i].substring(length);
-        let splitIndex = _i * 2
-        lolArr[splitIndex] = firstHalf
-        lolArr[splitIndex + 1] = secondHalf
-      })
-      newArr = lolArr
-    }
-    return newArr
+  splitSegment = (_segments, splitKey) => {
+    let newSegments = [ ..._segments ]
+    let segment = newSegments[splitKey]
+    let str = segment.str
+    let length = Math.ceil(str.length/2)
+    let _1st = str.substring(0, length);
+    let _2nd = str.substring(length);
+    newSegments.splice(splitKey, 1)
+    newSegments.splice(splitKey, 0, { str: _1st })
+    newSegments.splice(splitKey+1, 0, { str: _2nd})
+    let updateStopper = this.state.updateStopper+1
+    this.setState({ segments: newSegments, updateStopper })
   }
-
+ 
   componentDidMount() {
-    let children = this.props.children
-    let traversed = this.traverse([children])
-    // this.setState({
-    //   splitted: children.split(" ")
-    // })
-    this.setState({
-      text: traversed
-    })
+    let segments = [ { str: this.props.children} ]
+    this.splitSegment(segments, 0)
+  }
+
+  fineSearch = (_segments, fine) => {
+    let newSegments = [ ..._segments ]
+    let segment = newSegments[fine]
+    let str = newSegments[fine].str
+    let indexOf = str.indexOf(' ') + 1
+    let _1st = str.substring(0, indexOf)
+    let _2nd = str.substring(indexOf, str.length)
+    newSegments[fine - 1] = { str: newSegments[fine-1].str + _1st, className: 'fine' }
+    newSegments[fine] = { str: _2nd, className: 'fine' }
+    let updateStopper = this.state.updateStopper+1
+    this.setState({ segments: newSegments, fine, updateStopper })
   }
 
   componentDidUpdate(nextProps, nextState) {
-    let elem = this.refs.truncated
-    let children = elem.childNodes
-    let parent = this.refs.truncated.parentNode
-    let parentBottomY = parent.offsetTop + parent.offsetHeight
-    let splitKey
-    let prevY
-    let child
-    let isFine = this.state.isFine
-    let newText = [ ...this.state.text ]
-    let preventInfiniteLoop
-    if(this.state.preventInfiniteLoop<10) {
-      if (!isFine) {
-        for (let i = 0; i < children.length; i++) {
-          child = children[i]
-          if (child.offsetTop===prevY&&!isFine) {
-            // let xx = (_i, str) => {
-            //   if (children[_i + 1 ].offsetTop>children[_i].offsetTop) {
-            //     return str + newText[_i + 1].split(' ')[0]
-            //   } else {
-            //     let newStr = str + newText[_i + 1]
-            //     return xx(_i + 1, newStr)
-            //   }
-            // }
-            // let combined = xx(i - 1 , newText[i - 1])
-            // let split = combined.split(" ").map((val) => val + ' ' )
-            // newText.splice(i - 1 )
-            // newText = [...newText, ...split ]
-            isFine = true
-            preventInfiniteLoop = 100
+    setTimeout(() => {
+      let parent = this.refs.truncated.parentNode
+      let parentBottomY = parent.offsetTop + parent.offsetHeight
+      let childNodes = this.refs.truncated.childNodes
+      let fine = this.state.fine
+      let segments = [ ...this.state.segments]
+      if (fine&&!this.state.concatenated) {
+        if (childNodes[fine].offsetTop===childNodes[fine-1].offsetTop) {
+          this.fineSearch(segments, fine)
+        } else {
+          segments.splice(fine, segments.length)
+          let concatenated = ''
+          segments.map((segment) => {
+            concatenated = concatenated + segment.str
+          })
+          concatenated = concatenated.substring(0, concatenated.length - 3) + '...'
+          this.setState({ concatenated })
+        }
+      } else if (!this.state.concatenated) {
+        let prevY
+        for (let i = 0; i < childNodes.length; i++) {
+          let child = childNodes[i]
+          if (prevY===child.offsetTop) {
+            this.fineSearch(segments, i)
             break
           } else if (parentBottomY<child.offsetTop) {
-            splitKey = i - 1
-            let str = this.state.text[splitKey]
-            let newSplit = this.traverse([str])
-            //newText.splice(splitKey, 1)
-            newSplit.map((val, key) => {
-              newText.splice(splitKey+key, 0, val)
-            })
+            let splitKey = i - 1
+            this.splitSegment(segments, splitKey)
             break
           }
-          preventInfiniteLoop = this.state.preventInfiniteLoop + 1
           prevY = child.offsetTop
         }
-      } 
-      this.setState({
-        text: newText,
-        preventInfiniteLoop,
-        isFine
-      })
-    }
+      }
+    }, 300)
   }
 
-  getSpans = (text) => {
+  getSpans = (segments) => {
     let elems = []
-    text.map((str, i) => {
-      elems.push(<trnc-seg i={i}>{str}</trnc-seg>)
+    segments.map((o, i) => {
+      elems.push(<trnc-seg i={i} offset={o.offset} class={ o.className ? o.className : '' }>{o.str}</trnc-seg>)
     })
 
     return elems
   }
 
   render() {
-    let text = this.state.text
-
+    console.log('index.js', 'RENDERZ', 'Log here');
+    let segments = this.state.segments
+    let concatenated = this.state.concatenated
     return (
       <trnc-wrap ref="truncated">
         {
-          text
-          ? this.getSpans(text)
-          : null
+          concatenated
+          ? concatenated
+          : this.getSpans(segments)
         }
       </trnc-wrap>
     );
@@ -140,3 +126,21 @@ class Truncator extends React.Component {
 };
 
 export default Truncator;
+
+    // let newArr = [ ...arr ]
+    // let lolArr
+    // lolArr = []
+    // newArr.map((item, _i) => {
+    //   let size = newArr[_i]
+    //   let str = newArr[_i].str
+    //   let length = Math.ceil(str.length/2)
+    //   let firstHalf = str.substring(0, length);
+    //   let secondHalf = str.substring(length);
+    //   let splitIndex = _i * 2
+    //   lolArr[splitIndex] = { str: firstHalf, id: 'will have original id' }
+    //   lolArr[splitIndex + 1] = { str: secondHalf, id: 'will have new id' }
+    // })
+    // newArr = lolArr
+    // return newArr
+
+    // let parentBottomY = parent.offsetTop + parent.offsetHeight
